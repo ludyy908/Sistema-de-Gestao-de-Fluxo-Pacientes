cf92bbe3ef3326c0794c2879b77d06d87b77df9d
Wirzio <wirziobaloi1234@gmail.com>
2022-01-19 11:45:58 +0200

    Actualizacao da base de dados//insersao da tabela consulta_medico

--- Clinica/src/clinicapoo.sql
+++ Clinica/src/clinicapoo.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
 --
--- Host: localhost    Database: clinicapoo
+-- Host: 127.0.0.1    Database: clinicaoop
 -- ------------------------------------------------------
--- Server version	8.0.27
+-- Server version	8.0.26
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -77,16 +77,12 @@ DROP TABLE IF EXISTS `cirurgia`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `cirurgia` (
   `numeroCirurgia` int NOT NULL,
-  `codPaciente` int NOT NULL,
-  `nomePac` varchar(30) DEFAULT NULL,
-  `nomeMed` varchar(30) DEFAULT NULL,
-  `data` date DEFAULT NULL,
-  `hora` time DEFAULT NULL,
-  `estado` varchar(20) DEFAULT NULL,
+  `data` date NOT NULL,
+  `Hora` time NOT NULL,
+  `Estado` varchar(30) NOT NULL,
   PRIMARY KEY (`numeroCirurgia`),
-  KEY `codPaciente` (`codPaciente`),
-  CONSTRAINT `cirurgia_ibfk_1` FOREIGN KEY (`codPaciente`) REFERENCES `paciente` (`codPaciente`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  UNIQUE KEY `numeroCirurgia` (`numeroCirurgia`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -95,6 +91,7 @@ CREATE TABLE `cirurgia` (
 
 LOCK TABLES `cirurgia` WRITE;
 /*!40000 ALTER TABLE `cirurgia` DISABLE KEYS */;
+INSERT INTO `cirurgia` VALUES (666661,'0000-00-00','00:00:00',''),(666662,'0000-00-00','00:00:00','');
 /*!40000 ALTER TABLE `cirurgia` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -107,16 +104,11 @@ DROP TABLE IF EXISTS `consulta`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `consulta` (
   `numeroConsulta` int NOT NULL,
-  `codPaciente` int NOT NULL,
-  `nomePac` varchar(30) NOT NULL,
-  `nomeMed` varchar(30) NOT NULL,
-  `data` date DEFAULT NULL,
-  `hora` time DEFAULT NULL,
-  `estado` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`numeroConsulta`),
-  KEY `codPaciente` (`codPaciente`),
-  CONSTRAINT `consulta_ibfk_1` FOREIGN KEY (`codPaciente`) REFERENCES `paciente` (`codPaciente`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `Data` date NOT NULL,
+  `Hora` time NOT NULL,
+  `Estado` varchar(30) NOT NULL,
+  PRIMARY KEY (`numeroConsulta`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -125,6 +117,7 @@ CREATE TABLE `consulta` (
 
 LOCK TABLES `consulta` WRITE;
 /*!40000 ALTER TABLE `consulta` DISABLE KEYS */;
+INSERT INTO `consulta` VALUES (600001,'2022-01-15','11:15:15',''),(600002,'2021-01-15','10:15:15',''),(600003,'2022-01-05','05:15:15',''),(600004,'2022-01-05','05:55:15',''),(600005,'2022-01-05','15:55:15',''),(600006,'2022-01-13','15:45:15','');
 /*!40000 ALTER TABLE `consulta` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -152,6 +145,30 @@ LOCK TABLES `consulta_funcionario` WRITE;
 /*!40000 ALTER TABLE `consulta_funcionario` DISABLE KEYS */;
 INSERT INTO `consulta_funcionario` VALUES (600001,202001,'0000-00-00','00:00:00'),(600002,202001,'0000-00-00','00:00:00'),(600003,202005,'0000-00-00','00:00:00'),(600004,202001,'0000-00-00','00:00:00'),(600005,202001,'0000-00-00','00:00:00'),(600006,202005,'0000-00-00','00:00:00');
 /*!40000 ALTER TABLE `consulta_funcionario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `consulta_medico`
+--
+
+DROP TABLE IF EXISTS `consulta_medico`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `consulta_medico` (
+  `codFuncionario` int NOT NULL,
+  `numeroConsulta` int NOT NULL,
+  PRIMARY KEY (`numeroConsulta`,`codFuncionario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `consulta_medico`
+--
+
+LOCK TABLES `consulta_medico` WRITE;
+/*!40000 ALTER TABLE `consulta_medico` DISABLE KEYS */;
+INSERT INTO `consulta_medico` VALUES (202002,600001),(202003,600002);
+/*!40000 ALTER TABLE `consulta_medico` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -253,7 +270,7 @@ CREATE TABLE `funcionario` (
 
 LOCK TABLES `funcionario` WRITE;
 /*!40000 ALTER TABLE `funcionario` DISABLE KEYS */;
-INSERT INTO `funcionario` VALUES (27,'Gloria Uamusse','Femenino','11111111','Fomento','Mocambicana','Pediatria'),(68,'Ana Maria','Femenino','1236554789','Matola C','Mocambicana','Pediatria'),(112,'Ana Maria da Graca','Femenino','847689409','Fomento','Cubana','xxxxxxx'),(157,'Marcos Antonio','Masculino','33333333','Fomento','Gana','Estomatologia'),(183,'Camila Lucas','Femenino','87251878','Matola','Russa','cardiologia'),(189,'Mateus','Masculino','4444444','Fomento','Moz','CArio'),(207,'Lucas Amancio','Masculino','8457654','Matola','Russa','Cardiologia'),(212,'','Femenino','','','',''),(572,'Vania Amancio','Femenino','84759415','Ndlavela','Gana','Ortopedia'),(626,'Miguel','Masculino','2222222','Maputo','Russa','Ortopedia'),(926,'Graca Maria','Femenino','847689409','MAtola','Russa','Cardiologia'),(202001,'Amarilda','F','1000000000','','',''),(202002,'Celestino','M','100000001','','',''),(202003,'Kelvin','M','100000002','','',''),(202004,'Ludi','F','100000003','','',''),(202005,'Agostinho','M','100000004','','',''),(202006,'Faira','F','100000005','','','');
+INSERT INTO `funcionario` VALUES (27,'Gloria Uamusse','Femenino','11111111','Fomento','Mocambicana','Pediatria'),(68,'Ana Maria','Femenino','1236554789','Matola C','Mocambicana','Pediatria'),(73,'dscdscs','Masculino','845223896','sdcsdcdsc','dssdvsffv','sdcdscdsc'),(93,'xcvbn','Femenino','665135165','Magude','Moz','Producao'),(98,'sdcdsc','Masculino','845223896','dscdsc','sdcdscd','dscdscdscd'),(112,'Ana Maria da Graca','Femenino','847689409','Fomento','Cubana','xxxxxxx'),(141,'vhvhgg','Femenino','468966jbj','mgcyhg','nbvhvjhbjh','mnn,m,m'),(157,'Marcos Antonio','Masculino','33333333','Fomento','Gana','Estomatologia'),(183,'Camila Lucas','Femenino','87251878','Matola','Russa','cardiologia'),(189,'Mateus','Masculino','4444444','Fomento','Moz','CArio'),(207,'Lucas Amancio','Masculino','8457654','Matola','Russa','Cardiologia'),(212,'','Femenino','','','',''),(217,'dewdewd','Masculino','845223896','ssadsad','sadde','sadsads'),(228,'xcdacdc','Masculino','845223896','adafdf','dcdscdsc','adfadfadf'),(278,'qwdsd','Masculino','845223896','saddaa','adada','afsfffefe'),(309,' cx cxc cx','Masculino','cx cx ','acdacasdc','xc cx ','dcscscs'),(414,'frfer','Masculino','845223896','dwedwed','dfrferf','dwedewd'),(489,'mdrttgu','Masculino','59656hv','mhhfyu8','mvghvhvch','uhggggfh'),(572,'Vania Amancio','Femenino','84759415','Ndlavela','Gana','Ortopedia'),(626,'Miguel','Masculino','2222222','Maputo','Russa','Ortopedia'),(658,'dewdewd','Femenino','845223896','ewdewdew','wedewdew','ewdewdew'),(882,'saxasxas','Masculino','845223896','ASAs','xasasxsa','DFDFDFD'),(926,'Graca Maria','Femenino','847689409','MAtola','Russa','Cardiologia'),(202001,'Amarilda','F','1000000000','','',''),(202002,'Celestino','M','100000001','','',''),(202003,'Kelvin','M','100000002','','',''),(202004,'Ludi','F','100000003','','',''),(202005,'Agostinho','M','100000004','','',''),(202006,'Faira','F','100000005','','','');
 /*!40000 ALTER TABLE `funcionario` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -294,9 +311,10 @@ DROP TABLE IF EXISTS `medico`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `medico` (
-  `codFuncionario` int DEFAULT NULL,
-  `Especializacao` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `codFuncionario` int NOT NULL,
+  `Especializacao` varchar(30) NOT NULL,
+  PRIMARY KEY (`codFuncionario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -305,7 +323,7 @@ CREATE TABLE `medico` (
 
 LOCK TABLES `medico` WRITE;
 /*!40000 ALTER TABLE `medico` DISABLE KEYS */;
-INSERT INTO `medico` VALUES (202002,'Cardiologista'),(202003,'Oftalmologista'),(202002,'Cardiologista'),(202003,'Oftalmologista'),(112,'xxxxxxx');
+INSERT INTO `medico` VALUES (73,'sdcsdcdsc'),(93,'Enfermeiro'),(96,'hjgfdsdfgh'),(98,'sdcdscds'),(112,'Cortador'),(141,'jn jhjh'),(217,'asdsadsa'),(228,'afafadf'),(278,'Historia'),(309,'cdsacdsc'),(414,'Morning'),(489,'gvhgvv'),(658,'ewdewdew'),(882,'SDSDC'),(202002,'Cardiologista'),(202003,'Oftalmologita');
 /*!40000 ALTER TABLE `medico` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -362,7 +380,7 @@ CREATE TABLE `paciente` (
 
 LOCK TABLES `paciente` WRITE;
 /*!40000 ALTER TABLE `paciente` DISABLE KEYS */;
-INSERT INTO `paciente` VALUES (123,'Lina ','Femenino','Malhazine',7,'314848448484','Casado(a)','MOz','5555555','55555','Chihepe'),(210,'aaaa','Masculino','aaa',4,'aaa','Solteiro(a)','aaa','aaa','aaaa','aaaa'),(263,'aaaaa','Masculino','aaaaaa',15,'aaaaa','Solteiro(a)','aaaaa','11111','22222','aaaaa'),(418,'bbb','Masculino','bfff',5,'111111','Solteiro(a)','cccc','Masculino','Masculino','aaaa'),(755,'aaa','Masculino','aaa',5,'a','Solteiro(a)','aaa','a','a','aaaaa'),(763,'Americo','Masculino','Matola',8,'123654','Solteiro(a)','Russa','789654','45698','Julio'),(808,'Ludimila','Femenino','Mavotas',5,'789654123','Casado(a)','Russa','1265','79895','Mucevel'),(555551,'Antonio','M','Magude',0,'','','',NULL,NULL,''),(555552,'Jonas','M','Xinavane',0,'','','',NULL,NULL,''),(555553,'Marta','F','Motaze',0,'','','',NULL,NULL,''),(555554,'Maria','F','Duku',0,'','','',NULL,NULL,''),(555555,'Esperanca','F','Santo Antonio',0,'','','',NULL,NULL,''),(555556,'Cristina','F','Makupe',0,'','','',NULL,NULL,''),(555557,'Lucas','M','Mahotas',0,'','','',NULL,NULL,''),(555558,'Larissa','F','Aeroporto',0,'','','',NULL,NULL,'');
+INSERT INTO `paciente` VALUES (123,'Lina ','Femenino','Malhazine',7,'314848448484','Casado(a)','MOz','5555555','55555','Chihepe'),(210,'aaaa','Masculino','aaa',4,'aaa','Solteiro(a)','aaa','aaa','aaaa','aaaa'),(263,'aaaaa','Masculino','aaaaaa',15,'aaaaa','Solteiro(a)','aaaaa','11111','22222','aaaaa'),(706,'abelina','Masculino','JUNIOR',0,'110205463863s','Solteiro(a)','Moanswb','855223589','541236862',''),(755,'aaa','Masculino','aaa',5,'a','Solteiro(a)','aaa','a','a','aaaaa'),(763,'Americo','Masculino','Matola',8,'123654','Solteiro(a)','Russa','789654','45698','Julio'),(808,'Ludimila','Femenino','Mavotas',5,'789654123','Casado(a)','Russa','1265','79895','Mucevel'),(555551,'Antonio','M','Magude',0,'','','',NULL,NULL,''),(555552,'Jonas','M','Xinavane',0,'','','',NULL,NULL,''),(555553,'Marta','F','Motaze',0,'','','',NULL,NULL,''),(555554,'Maria','F','Duku',0,'','','',NULL,NULL,''),(555555,'Esperanca','F','Santo Antonio',0,'','','',NULL,NULL,''),(555556,'Cristina','F','Makupe',0,'','','',NULL,NULL,''),(555557,'Lucas','M','Mahotas',0,'','','',NULL,NULL,''),(555558,'Larissa','F','Aeroporto',0,'','','',NULL,NULL,'');
 /*!40000 ALTER TABLE `paciente` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -512,17 +530,9 @@ CREATE TABLE `secretario` (
 
 LOCK TABLES `secretario` WRITE;
 /*!40000 ALTER TABLE `secretario` DISABLE KEYS */;
-INSERT INTO `secretario` VALUES (202001,'Amarilda','amarildachihepe@gmail.com','Secretario',1);
+INSERT INTO `secretario` VALUES (202001,'Amarilda','amarildachihepe@gmail.com','Secretario',1),(1502,'Baloi','baloi@gmail.com','Professor',2);
 /*!40000 ALTER TABLE `secretario` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Dumping events for database 'clinicapoo'
---
-
---
--- Dumping routines for database 'clinicapoo'
---
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -533,4 +543,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-01-19  0:02:20
+-- Dump completed on 2022-01-19 11:36:29
